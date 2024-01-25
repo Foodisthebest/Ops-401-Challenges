c74e1c720ee4564d88f3930c7d77c37011659458
woodstock <nonaandnea@yahoo.com>
2024-01-24 19:40:26 -0800

    In progress.

--- opschallenge11.py
+++ opschallenge11.py
@@ -2,7 +2,7 @@
 
 # Script:                     Network Scanning with Scapy
 # Author:                     Renona Gay
-# Date of latest revision:   1/23/2024 
+# Date of latest revision:    1/23/2024 
 # Purpose:                    Ops 401 Challenge 11
 
 #!/usr/bin/env python3
@@ -10,10 +10,25 @@
 
 #! /usr/bin/env python
 
+# These two lines imports the python system (python itself) so that you can 
+# use it to make the code more adaptable, exit the program safely or handle errors "more gracefully"
+# and makes the contents of sys readable on the screen.
 import sys
-from scapy.all import sr1,IP,ICMP
+
+# This line 
+from scapy.all import Ether, IP, sniff, ARP, sr1, ICMP, TCP
 
 p=sr1(IP(dst=sys.argv[1])/ICMP())
 if p:
     p.show()
 
+
+
+
+
+
+
+
+
+# Resources: https://g.co/bard/share/9950a8198b90
+# https://github.com/codefellows/seattle-cybersecurity-401d10/blob/main/class-11/challenges/DEMO.md
---
+++ opschallenge13.py
@@ -0,0 +1,9 @@
+# !/bin/bash/python
+
+# Using as placeholder until I can get to this 100% from part 1
+
+import ipaddress
+
+# Creating an object of IPv4Address class and
+# initializing it with an IPv4 address.
+ip = ipaddress.IPv4Address('112.79.234.30')

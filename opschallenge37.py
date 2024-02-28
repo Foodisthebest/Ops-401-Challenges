513467062096bb7650f713f8b622ba12b5cd7623
woodstock <nonaandnea@yahoo.com>
2024-02-27 17:57:44 -0800

    In progress.

---
+++ opschallenge37.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Script Name: Cookie Capture Capades
+# Author: Renona Gay, using provided script for this challenge
+# Date of latest revision: 02/27/2024
+# Purpose: To capture a cookie and send it back out to the site in order to receive a valid response in HTTP text, all using Python’s Requests module.
+
+# PLACEHODER FOR FINAL ASSIGNMENT
+
+# The below Python script shows one possible method to return the cookie from a site that supports cookies.
+
+import requests
+
+# targetsite = input("Enter target site:") # Uncomment this to accept user input target site
+targetsite = "http://www.whatarecookies.com/cookietest.asp" # Comment this out if you're using the line above
+response = requests.get(targetsite)
+cookie = response.cookies
+
+def bringforthcookiemonster(): # Because why not!
+    print('''
+
+              .---. .---.
+             :     : o   :    me want cookie!
+         _..-:   o :     :-.._    /
+     .-''  '  `---' `---' "   ``-.
+   .'   "   '  "  .    "  . '  "  `.
+  :   '.---.,,.,...,.,.,.,..---.  ' ;
+  `. " `.                     .' " .'
+   `.  '`.                   .' ' .'
+    `.    `-._           _.-' "  .'  .----.
+      `. "    '"--...--"'  . ' .'  .'  o   `.
+
+        ''')
+
+bringforthcookiemonster()
+print("Target site is " + targetsite)
+print(cookie)
+
+# Add here some code to make this script perform the following:
+# - Send the cookie back to the site and receive a HTTP response
+# - Generate a .html file to capture the contents of the HTTP response
+# - Open it with Firefox
+#
+# Stretch Goal
+# - Give Cookie Monster hands
